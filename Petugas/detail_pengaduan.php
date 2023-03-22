44c319bdadbe12d918465497241983a7095906a7
Nata Inditama <natainditama@gmail.com>
2023-03-23 05:47:08 +0800

    feat(petugas): Rename petugas directory

--- Petugas/detail_pengaduan.php
+++ Petugas/detail_pengaduan.php
@@ -1,51 +1,59 @@
 <?php
 $id = $_GET['id'];
-if(empty($id)){
-    header("location:masyarakat.php");
+if (empty($id)) {
+    echo "<script>
+    window.location.assign('petugas/index.php');
+    </script>";
 }
-include'../koneksi.php';
-$query = mysqli_query($koneksi, "SELECT*FROM pengaduan WHERE id_pengaduan='$id'");
-$data = mysqli_fetch_array($query);
+include __DIR__ . './../koneksi.php';
+$query = mysqli_query($koneksi, "SELECT * FROM pengaduan WHERE id_pengaduan='$id'");
+$data = mysqli_fetch_assoc($query);
 ?>
 <div class="card shadow">
     <div class="card-header">
-    <?php
-        require '../koneksi.php';
-        $sql=mysqli_query($koneksi, "SELECT * FROM pengaduan WHERE id_pengaduan='$_GET[id]'");
-        $data=mysqli_fetch_array($sql);
-        if($sql)
-        ?>
-        <a href="?url=verifikasi_pengaduan" class="btn btn-primary btn-icon-split"> 
-        <span class=" icon text-white-5">
-            <i class="fa fa-arrow-left"></i>
-        </span>
-        <span class="text">kembali</span>
-    </a>
-</div>
-<div class="card-header">
-        <a href="proses.php?id=<?= $data['id_pengaduan'] ?>" class="btn btn-danger btn-icon-split" onclick="return confirm('Yakin Akan Di Proses?')"> 
-        <span class=" icon text-white-5">
-            <i class="fa fa-check"></i>
-        </span>
-        <span class="text">Verifikasi</span>
-    </a>
-</div>
-<div class="card-body">
-<form  method="post" action="proses_pengaduan.php" enctype="multipart/form-data">
-
-<div class="form-group">
-    <label style="font-size: 18px">Tanggal Pengaduan</label><br>
-    <input type="text" name="tgl_pengaduan" class="form_control" readonly value="<?=$data['tgl_pengaduan'];?>">
-</div>
-<div class="form-group">
-    <label style="font-size: 18px">Bukti Foto : </label><br>
-    <img class="img-thumbnail"  img src="../foto/<?= $data['foto']; ?>" width="800">
-</div>
-<div class="form-group">
-    <label style="font-size: 18px">Isi Laporan : </label>
-    <textarea name="isi_laporan" class="form-control" readonly value required><?=$data['isi_laporan']?></textarea>
-</div>
-</form> 
-
-</div>
+        <a href="petugas/index.php?url=lihat_pengaduan" class="btn btn-primary btn-icon-split">
+            <span class=" icon text-white-5">
+                <i class="fa fa-arrow-left"></i>
+            </span>
+            <span class="text">kembali</span>
+        </a>
+    </div>
+    <div class="card-body">
+        <form method="post" action="petugas/proses_pengaduan.php" enctype="multipart/form-data">
+            <input type="hidden" name="id_pengaduan" value="<?= $data['id_pengaduan']; ?>" readonly>
+            <div class="form-group">
+                <label style="font-size: 18px">Status : </label><br>
+                <select name="status" class="form-control" readonly id="status">
+                    <?php
+                    if ($data['status'] == '0') {
+                        echo "<option value=" . $data['status'] . " selected>Menunggu</option>";
+                    } elseif ($data['status'] == 'proses') {
+                        echo "<option value=" . $data['status'] . " selected>Proses</option>";
+                    } elseif ($data['status'] == 'selesai') {
+                        echo "<option value=" . $data['status'] . " selected>Selesai</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label style="font-size: 18px">Tanggal Pengaduan</label><br>
+                <input type="text" name="tgl_pengaduan" class="form-control" readonly value="<?= $data['tgl_pengaduan']; ?>">
+            </div>
+            <div class="form-group">
+                <label style="font-size: 18px">Isi Laporan : </label>
+                <textarea name="isi_laporan" readonly class="form-control " rows="5" required><?= $data['isi_laporan'] ?></textarea>
+            </div>
+            <div class="form-group">
+                <label style="font-size: 18px">
+                    <p>Foto :</p>
+                    <img class="img-thumbnail" src="foto/<?= $data['foto'] ?>" width="350">
+                    <input type="file" style="display: none;" name="foto" readonly disabled accept="image/*">
+                </label>
+            </div>
+            <!-- <div class="form-group">
+                <button type="submit" name="update" class="btn btn-success mt-2">Simpan</button>
+                <button type="submit" name="delete" class="btn btn-danger mt-2">Hapus</button>
+            </div> -->
+        </form>
+    </div>
 </div>

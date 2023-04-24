6ee70ebb9f15252fc95fa79a23b1bf5748e0247d
Nata Inditama <natainditama@gmail.com>
2023-04-24 20:44:13 +0800

    style; Update layouts page

--- masyarakat/tanggapan.php
+++ masyarakat/tanggapan.php
@@ -11,34 +11,34 @@ if (!empty($_GET['id_pengaduan'])) {
 ?>
     <div class="container">
         <div class="row">
-            <div class="col-md-12" mt-3>
+            <div class="d-sm-flex align-items-center justify-content-between pt-4">
+                <h1 class="h3 mb-0 text-gray-800">
+                    Data Tanggapan
+                </h1>
             </div>
-            <div class="card">
-                <div class="card-header">
-                    LIHAT TANGGAPAN
-                </div>
-                <div class="card-body">
-                    <form action="" method="POST">
-                        <div class="mb-3">
-                            <label class="form-label">Judul Laporan</label>
-                            <input type="text" class="form-control" value="<?php echo $data['judul_laporan'] ?>" readonly>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Isi Laporan</label>
-                            <textarea class="form-control" readonly><?php echo $data['isi_laporan'] ?></textarea>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Foto</label>
-                            <img src="../assets/img/<?php echo $data['foto'] ?>" class="form-control" style="width: 150px;">
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Tanggapan</label>
-                            <textarea class="form-control" readonly><?php echo $data['tanggapan'] ?></textarea>
-                        </div>
-                        <div class="card-footer">
+            <div class="col-md-12 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="mb-3">
+                                <label class="form-label">Judul Laporan</label>
+                                <input type="text" class="form-control" value="<?php echo $data['judul_laporan'] ?>" readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Isi Laporan</label>
+                                <textarea class="form-control" readonly><?php echo $data['isi_laporan'] ?></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Foto</label>
+                                <img src="../assets/img/<?php echo $data['foto'] ?>" width="100" class="form-control col-3" style="height:auto;" readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Tanggapan</label>
+                                <textarea class="form-control" readonly><?php echo $data['tanggapan'] ?></textarea>
+                            </div>
                             <a href="index.php" class="btn btn-primary">KEMBALI</a>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
